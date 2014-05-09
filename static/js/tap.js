e8e03799bd91d46ad246b4a960e6d3fddd2ecec5
max-wittmann <damxam@gmail.com>
2014-05-09 17:21:17 +1000

    Trying to mod tap to use gestures

--- static/js/tap.js
+++ static/js/tap.js
@@ -8,20 +8,27 @@ $(document).ready(function () {
         socket.emit('message', 'Message Sent on ' + new Date());
     });
 
-    $(window).on("touchstart", function(ev) {
-        var e = ev.originalEvent;
-        console.log("Touched " + e.touches);
+    $(window).bind("touchstart touchmove touchend", function(evt){
+//        $(box).text(evt.type + ", " + " touchstart called");
+        socket.emit('message', 'Message Sent on ' + new Date());
+        evt.preventDefault();
+        $(box).css("backgroundColor", "blue");
     });
 
-    $(window).on("touchend", function(ev) {
-        var e = ev.originalEvent;
-        console.log("Touched " + e.touches);
-    });
-
-    $(window).on("click", function(ev) {
-        var e = ev.originalEvent;
-        console.log("Clicked " + e.touches);
-    });
+//    $(window).on("touchstart", function(ev) {
+//        var e = ev.originalEvent;
+//        console.log("Touched " + e.touches);
+//    });
+//
+//    $(window).on("touchend", function(ev) {
+//        var e = ev.originalEvent;
+//        console.log("Touched " + e.touches);
+//    });
+//
+//    $(window).on("click", function(ev) {
+//        var e = ev.originalEvent;
+//        console.log("Clicked " + e.touches);
+//    });
 
     $("sender").bind("tap", tapHandler);
 
